28f8bb14cc32623ea4fa671d4af2758e8fe8a5c9
App Academy Student <email@email.com>
2022-03-29 15:27:04 -0400

    finished tic tac toe

--- vanilla_tic_tac_toe/src/index.js
+++ vanilla_tic_tac_toe/src/index.js
@@ -1,6 +1,12 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js"); // require appropriate file
+const Game = require("../ttt_node/game.js"); // require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  let g = new Game();
+  let container = document.querySelector(".ttt");
+  let view = new View(g,container);
+
+
+
 });
--- vanilla_tic_tac_toe/src/ttt-view.js
+++ vanilla_tic_tac_toe/src/ttt-view.js
@@ -1,13 +1,69 @@
+const Game = require("../ttt_node/game");
+
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    el.appendChild(this.setupBoard());
+    this.bindEvents();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    let ul = document.createElement('ul'); 
+    ul.setAttribute("class", "grid-parent");
+    let j = 0;
+    for(let i = 0; i < 9; i++) {
+      if(j === 3) j = 0;
+      let div = document.createElement('div');
+      let li = document.createElement('li');
+      div.setAttribute("id",i.toString());
+      div.setAttribute("data-index", [Math.floor(i/3), j].toString());
+      div.setAttribute("class", "grid-position");
+      ul.appendChild(div);
+      div.appendChild(li);
+      j++;
+    }
+    return ul;
+  }
   
-  bindEvents() {}
+  bindEvents() {
+    let ul = document.querySelector(".grid-parent");
+    ul.addEventListener("click", this.handleClick.bind(this));
 
-  handleClick(e) {}
 
-  makeMove(square) {}
+  }
+
+  handleClick(e) {
+    if (!this.game.isOver()){
+      let ele = e.target;
+      let square = ele.getAttribute("id");
+      this.makeMove(square);
+
+      let pos = ele.getAttribute("data-index").split(",");
+      pos[0] = parseInt(pos[0]);
+      pos[1] = parseInt(pos[1]);
+      this.game.playMove(pos);
+      if(this.game.isOver()){
+        let message = document.createElement("h3");
+        message.classList.add('win-message');
+        message.innerText = `You win, ${this.game.winner()}`;
+        this.el.appendChild(message);
+      }
+    }
+  }
+
+  makeMove(square) {
+    let li = document.getElementById(square);
+    if (li.classList.contains("x") || li.classList.contains("o")) {
+      alert("Invalid Move!");
+    } else if (this.game.currentPlayer === 'x') {
+      li.classList.add("x");
+      li.innerText = 'X'
+    } else {
+      li.classList.add("o");
+      li.innerText = 'O';
+    }
+  }
 
 }
 
